d1c7460ab8aa403fc1410c2fb2eed03566d35363
piorunzf <zenon.fendryk@wp.pl>
2020-01-30 13:39:13 +0100

    instlacja freamwork

---
+++ Afore2020/Models/AWojewodztwo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Afore2020.Models
+{
+    public class AWojewodztwo
+    {
+
+        [Key]
+        public int AWojewodztwoID { get; set; }
+
+        [StringLength(70)]
+        [Display(Name = "Nazwa województwa")]
+
+        public string AWojewodztwoNazwa { get; set; }
+
+    }
+}
