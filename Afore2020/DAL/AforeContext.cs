e41ddeedcb1ae0fb1eaabf30660cfbf182fff348
piorunzf <zenon.fendryk@wp.pl>
2020-01-30 14:07:03 +0100

    dodaniePolaczenia

---
+++ Afore2020/DAL/AforeContext.cs
@@ -0,0 +1,29 @@
+﻿using Afore2020.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Afore2020.DAL
+{
+    public class AforeContext : DbContext
+{
+        public AforeContext() :base("AforeBaza")
+            {
+            }
+
+
+        //usowa s na koncach wtorzonych tabelach
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+
+        public DbSet<AWojewodztwo>  wojewodztwos { get; set; }
+
+    }
+}
